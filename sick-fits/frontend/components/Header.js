28ad0682ef13a30ec2bec0d788f512f68bd20a2b
Miguel Bolivar <MBolivar360@gmail.com>
2018-10-19 11:59:00 -0400

    Adding global styles for typography and also, including navStyles by Wes Bos

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -15,10 +15,33 @@ const Logo = styled.h1`
     text-transform: uppercase;
     text-decoration: none;
   }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${({ theme }) => theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      ustify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${({ theme }) => theme.lightGrey};
+  }
 `;
 
 const Header = props => (
-  <div>
+  <StyledHeader>
     <div className="bar">
       <Logo>
         <Link href="/">
@@ -33,7 +56,7 @@ const Header = props => (
     <div>
       Cart
     </div>
-  </div>
+  </StyledHeader>
 
 );
 
