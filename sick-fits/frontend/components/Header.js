bf4c978133537b3410f2facae739e2e751c62d1e
Miguel Bolivar <MBolivar360@gmail.com>
2018-10-19 10:49:21 -0400

    Adding styled logo for H1 sick fits logo

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,9 +1,30 @@
 import Nav from './Nav';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${({ theme }) => theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+`;
 
 const Header = props => (
   <div>
     <div className="bar">
-      <a href="">Sick fits</a>
+      <Logo>
+        <Link href="/">
+          <a>Sick fits</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
