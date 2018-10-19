e4586e8e8a440764c3e9ff8dbe1725506b183a5f
Miguel Bolivar <MBolivar360@gmail.com>
2018-10-19 16:07:34 -0400

    Adding first query to API for items

---
+++ sick-fits/frontend/components/Items.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+class Items extends Component {
+
+  render() {
+    return(
+      <div>
+        <p>items</p>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({data, error, loading}) => {
+            if (loading) return <p>Loading...</p>
+            else if (error) return <p>{error}</p>
+            else return <p>I found {data.items.length} items</p>
+          }}
+        </Query>
+      </div>
+    );
+  }
+
+}
+
+export default Items;
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import Items from '../components/Items';
 
 const Home = props => (
   <div>
     <h1>Home from NEXT</h1>
+    <Items />
   </div>
 )
 
