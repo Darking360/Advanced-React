bd1c0fb98521c13b4c4783a43671ba81cd7f4ce6
Miguel Bolivar <MBolivar360@gmail.com>
2018-10-19 15:37:34 -0400

    Adding initial mutation and query in order to work with Prisma with prisma-binding. Creating and fetching items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,9 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO check if they're logged in
+    const item = await ctx.db.mutation.createItem({data: { ...args }}, info);
+    return item;
+  },
+};
 
 module.exports = mutations;
