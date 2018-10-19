265bff6af60c531db80154d1a29bf21afe20f96a
Miguel Bolivar <MBolivar360@gmail.com>
2018-10-19 10:38:41 -0400

    Adding theming to styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
-const Button = styled.button`
-  background: red;
-  font-size: ${({huge}) => huge ? '100px' : '50px'};
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${({ theme }) => theme.black};
+`;
+
+const PageSizer = styled.div`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 class Page extends Component {
@@ -13,12 +29,15 @@ class Page extends Component {
   render() {
     const { children } = this.props;
     return(
-      <div>
-        <Meta />
-        <Header />
-        <Button huge>Click me</Button>
-        { children }
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <PageSizer>
+            { children }
+          </PageSizer>
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
