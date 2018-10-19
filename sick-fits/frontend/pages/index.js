2ada334b8d4011346fa2a14026129e63dca47c3a
Miguel Bolivar <MBolivar360@gmail.com>
2018-10-19 10:15:35 -0400

    Adding initial components for Header and Nav, and refactor to separate components, and adding app layout

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -3,9 +3,6 @@ import Link from 'next/link';
 const Home = props => (
   <div>
     <h1>Home from NEXT</h1>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
   </div>
 )
 
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -3,9 +3,6 @@ import Link from 'next/link';
 const Sell = props => (
   <div>
     <h1>Sell from NEXT</h1>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
   </div>
 )
 
